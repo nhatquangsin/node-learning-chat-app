4212aca0bf8ec7f251a02d10997d1233b1d50947
Quang Truong <nhatquangsin@gmail.com>
2017-07-13 12:05:20 +0700

    Emit newMessage on creageMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function ()  {
     console.log('Connected to the server');
-
-    socket.emit('createMessage', {
-        to: 'jen@eample.com',
-        text: 'Hey. This is Quang'
-    });
 });
 
 socket.on('disconnect', function ()  {
